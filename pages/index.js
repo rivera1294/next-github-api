b8d991a7acd763fc800b20ac7974641a27467f5e
Jason Rivera <jrivera2850@gmail.com>
2020-07-29 22:43:45 -0500

    updated

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import useSWR from 'swr';
-import fetcher from '../utils/fetcher';
+// import fetcher from '../utils/fetcher';
 import { base_url } from '../utils/url'
 import {
   Flex,
@@ -16,47 +16,46 @@ import {
 } from "@chakra-ui/core";
 import { justifyContent } from 'styled-system';
 
+async function fetcher(...args) {
+  const res = await fetch(...args);
+
+  return res.json();
+}
+
 const Home = (props) => {
+  const { data } = useSWR('/api/github', fetcher);
+
   return (
     <>
       <Box mt={5}>
         <Heading as="h1" textAlign="center" size="2xl" mb={5}>Jason's Github Stats</Heading>
         <Flex justify="center">
           <Box display={{ md: "flex", sm: "block" }}>
-            <Box w="300px" p={5} ml={8} mb={3} borderWidth="1px" rounded="lg" overflow="hidden">
-              <Stat>
-                <StatLabel>
-                  <Text fontSize="xl">Github Stars</Text>
-                </StatLabel>
-                {/* <StatNumber>{props.data.stars}</StatNumber> */}
-              </Stat>
-            </Box>
-            <Box w="300px" p={5} ml={8} mb={3} borderWidth="1px" rounded="lg" overflow="hidden">
-              <Stat>
-                <StatLabel>
-                  <Text fontSize="xl">Github Followers</Text>
-                </StatLabel>
-                {/* <StatNumber>{props.data.followers}</StatNumber> */}
-              </Stat>
-            </Box>
+            {!data ? "loading" : (
+              <>
+                <Box w="300px" p={5} ml={8} mb={3} borderWidth="1px" rounded="lg" overflow="hidden">
+                  <Stat>
+                    <StatLabel>
+                      <Text fontSize="xl">Github Stars</Text>
+                    </StatLabel>
+                    <StatNumber>{data.stars}</StatNumber>
+                  </Stat>
+                </Box>
+                <Box w="300px" p={5} ml={8} mb={3} borderWidth="1px" rounded="lg" overflow="hidden">
+                  <Stat>
+                    <StatLabel>
+                      <Text fontSize="xl">Github Followers</Text>
+                    </StatLabel>
+                    <StatNumber>{data.followers}</StatNumber>
+                  </Stat>
+                </Box>
+              </>
+            )}
           </Box>
         </Flex>
       </Box>
     </>
   )
 }
-
-// export async function getStaticProps() {
-//   console.log('node', process.env.NODE_ENV)
-//   console.log('bae', base_url)
-//   const res = await fetch(`${base_url}/api/github`)
-//   const data = await res.json()
-
-//   return {
-//     props: {
-//       data,
-//     },
-//   }
-// }
 
 export default Home
